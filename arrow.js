6cd9f1320963ee6bc50cd7ca6d5b8fec0895064b
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-15 19:29:05 +0900

    Arrow Function

---
+++ arrow.js
@@ -0,0 +1,16 @@
+// var a = function name(n) {
+//     return n;
+// }
+// console.log(a('Easy Learning'));
+
+// let fruit = (f) => { return f };
+// console.log(fruit('Apple'));
+
+// let fruit = f => f;
+// console.log(fruit('Mango'));
+
+// let multiply = m => m * m * m;
+// console.log(multiply(3));
+
+let add = (m, n) => m + n;
+console.log(add(3, 2));
